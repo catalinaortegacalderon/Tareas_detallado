3c2ed16f4d56edaa09358563781502ac04ca0d84
tareas_detallado <105682990+catalinaortegacalderon@users.noreply.github.com>
2024-04-03 13:27:06 -0300

    Add files via upload

---
+++ Photoshop/Photoshop/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Para usar este código debes instalar "imagesharp"
+ * https://docs.sixlabors.com/articles/imagesharp/index.html
+ * Instalar esta librería es realmente fácil. Yo usé NuGet que aparece en la barra
+ * de abajo en Rider. Luego busqué la librería "SixLabors.ImageSharp" y puse instalar.
+ */
+
+using Photoshop;
+using Photoshop.Effects;
+
+IPhotoEffect[] availableEffects = new IPhotoEffect[] { new BlackAndWhiteEffect()};
+string folder = "imgs";
+Controller.Run(folder, availableEffects);
